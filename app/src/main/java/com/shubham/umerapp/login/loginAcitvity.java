e8726a39c6d500aa9f2ab37f2c4f394d272a359f
Shubham Raut <rautshubham280@gmail.com>
2024-01-07 15:29:41 +0530

    starting

---
+++ app/src/main/java/com/shubham/umerapp/login/loginAcitvity.java
@@ -0,0 +1,16 @@
+package com.shubham.umerapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shubham.umerapp.R;
+
+public class loginAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitvity);
+    }
+}

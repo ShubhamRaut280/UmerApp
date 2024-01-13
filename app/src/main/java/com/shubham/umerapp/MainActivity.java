7612dc5052f3934a10b271ef872473191a76c433
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2024-01-13 15:43:53 +0530

    Revert "Added date and time fetch (thread), fragments"

--- app/src/main/java/com/shubham/umerapp/MainActivity.java
+++ app/src/main/java/com/shubham/umerapp/MainActivity.java
@@ -2,39 +2,13 @@ package com.shubham.umerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.View;
-import android.widget.Button;
-
-import org.apache.commons.net.time.TimeTCPClient;
-
-import java.io.IOException;
-import java.util.concurrent.Executors;
-
-import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
-    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b = findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, UserHomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
-
 }
-

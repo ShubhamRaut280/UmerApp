df6d05c107cbd8e93ba9bfd649d6b41175405e0d
S-aurav <yadavsaurav818@gmail.com>
2024-01-17 19:09:46 +0530

    Added Calendar to fragment3

--- app/src/main/java/com/shubham/umerapp/User/Fragments/fragment3.java
+++ app/src/main/java/com/shubham/umerapp/User/Fragments/fragment3.java
@@ -1,40 +1,145 @@
 package com.shubham.umerapp.User.Fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shubham.umerapp.R;
-import com.shubham.umerapp.User.Adapters.MyAdapter;
+import com.shubham.umerapp.User.Adapters.CalendarAdapter;
+import com.shubham.umerapp.helperFunctions;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class fragment3 extends Fragment {
 
+public class fragment3 extends Fragment implements CalendarAdapter.OnItemListener {
+
+    public TextView monthYearText;
+    public RecyclerView calendarRecyclerView;
+    public LocalDate selectedDate;
+    CardView previousMonth, nextMonth;
+    String s_year, s_month, s_day;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
 
+        calendarRecyclerView = view.findViewById(R.id.calendarRecyclerView);
+        monthYearText = view.findViewById(R.id.monthYearTV);
+        previousMonth = view.findViewById(R.id.previous);
+        nextMonth = view.findViewById(R.id.next);
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        previousMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previousMonthAction(view);
+            }
+        });
 
-        List<Item> items = new ArrayList<Item>();
-        items.add(new Item(R.drawable.user));
-        items.add(new Item(R.drawable.user));
+        nextMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextMonthAction(view);
+            }
+        });
 
+        selectedDate = LocalDate.now();
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new MyAdapter(getContext(),items));
-
+        Toast.makeText(getContext(), "LOOOl: "+selectedDate, Toast.LENGTH_LONG).show();
+        Log.e("LMAOOO: ", selectedDate.toString());
+        setMonthView();
         return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+
+        String currDate;
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                daysInMonthArray.add("");
+            }
+            else
+            {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousMonthAction(View view)
+    {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextMonthAction(View view)
+    {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onItemClick(int position, String dayText)
+    {
+        if(!dayText.equals(""))
+        {
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+        }
     }
 }
